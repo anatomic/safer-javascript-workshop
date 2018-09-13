6d850dc9ba3e4ae87fcfca437d788bacda3d4518
Ian Thomas <ian.thomas@skybettingandgaming.com>
2018-09-13 15:07:38 +0100

    adds skeleton for workshop

---
+++ workshop/ex-4/main.js
@@ -0,0 +1,14 @@
+/**
+ * Using the CityBikes API (http://api.citybik.es/v2/):
+ * 1. Get all networks and filter down to just those in GB
+ * 2. Get all stations for each of the GB networks
+ * 3. Sum the number of free_bikes for each network
+ * 4. Output a list sorted by number of free bikes
+ */
+
+const app = () => {
+  console.error("Time to have a go at this exercise!!")
+  process.exit(1);
+};
+
+app();
